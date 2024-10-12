b0ab059e3400694d52e4a0d513c3e2abac55a05e
Kaushal <kaushal14707@gmail.com>
2024-10-13 00:54:58 +0530

    about us pop-up add

--- aboutus.js
+++ aboutus.js
@@ -1,4 +1,19 @@
+var pop_up = document.getElementById("about_us");
+var content = document.getElementById("content");
+
 function aboutus()
 {
+    pop_up.style.visibility = "visible";
+    pop_up.style.display = "flex"
     
+    content.style.filter = "blur(50px)";
+}
+
+//to close pop-up
+function closeabtus()
+{
+    
+    pop_up.style.visibility = "hidden";
+    pop_up.style.display = "none"
+    content.style.filter = "blur(0px)";
 }
